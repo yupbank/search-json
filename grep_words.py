3c047533613b5127fc992198e43513dff456f6d7
yupbank <yupbank@gmail.com>
2014-04-02 21:43:27 +0800

    ok

--- grep_words.py
+++ grep_words.py
@@ -13,18 +13,8 @@ from collections import defaultdict
 from math import log
 
 default_cut_length = 4
-word_to_user = defaultdict(set)
-user_word_count = defaultdict(lambda: defaultdict(int))
 word_freq = defaultdict(int)
-document_freq = defaultdict(int)
-word_tf_idf = defaultdict(float)
-word_by_length = defaultdict(set)
-term_probrablity = {}
-term_condition_probrablity = {}
-
-total_document = 0
 total_word = 0
-total_user = 0
 tire = {}
 inverse_tire = {}
 
@@ -53,26 +43,18 @@ inverse_tire_building = build_trie(inverse_tire)
 
 def document_to_word(document):
     for i in document:
+        i  = i.strip()
         for word in generate_by_cut(i): 
             trie_building(word)
             inverse_tire_building(word[::-1])
             yield word
 
-def count_tf_idf_uf(user, document):
-    global total_document
-    total_document += 1
-    in_document_words = set()
+def count_freq(document):
     for word in document_to_word(document):
         global total_word
-        word_by_length[len(word)].add(word)
         total_word += 1
-        word_to_user[word].add(user)
-        user_word_count[user][word] += 1
         word_freq[word] += 1
-        in_document_words.add(word)
 
-    for word in in_document_words:
-        document_freq[word] += 1
 
 def entropy(counts):
     total = float(sum(counts))
@@ -81,8 +63,6 @@ def entropy(counts):
         proba = i/total
         result = result - proba * log(proba)
     return result
-
-MAX = 10000000000
 
 
 def calculate_entorpy_right(word):
@@ -102,45 +82,32 @@ def calculate_entropy_left(word):
     for i in prefix:
         if i in atree:
             atree = atree[i]
-    scope = atree.keys()
-    counts = filter(lambda x: x, map(lambda x: word_freq[(word+x)[::-1]], scope))
+    scope = filter(lambda x: x, atree.keys())
+
+    #counts = filter(lambda x: x, map(lambda x: word_freq[(prefix+x)[::-1]], scope))
+    #print ' '.join(bitch)
+    counts = map(lambda x: word_freq[(prefix+x)[::-1]], scope)
     
     return entropy(counts)
 
 def main(): 
-    potential_words = defaultdict(int) 
-    assets = set()
     print 'start reading words....'
     with open('words') as f:
         for n, line in enumerate(f):
             line = line.decode('U8')
             line = line.strip().split(' ')
             user, document = line[0], line[1:]
-            count_tf_idf_uf(user, document)
-            print n
+            count_freq(document)
     print 'finish reading words....'
-    global total_user
-    total_user = len(user_word_count.keys())
     word_combination = {}
     word_freedom = {}
 
-
     print 'start aggregating results....'
-
-    for word in word_to_user:
+    words = word_freq.keys()
+    for word in words:
         if len(word) > 1:
             word_combination[word] = min(word_freq[word]/float(word_freq[word[:-1]]*word_freq[word[-1]]), word_freq[word]/float(word_freq[word[0]]*word_freq[word[1:]]))
             word_freedom[word] = min(calculate_entorpy_right(word), calculate_entropy_left(word))
-        tf = word_freq[word]/float(total_word)
-        idf = log(float(total_document)/document_freq[word])
-        word_tf_idf[word] = tf*idf
-
-    print 'finish aggregating results....'
-    top = [ [i, j] for i,j in word_tf_idf.iteritems()]
-    top.sort(key=lambda x:x[1], reverse=True)
-    with open('tf_idf', 'w') as f:
-        for i, j in top:
-            print >>f, i.encode('U8'), j
     
     print 'writing....'
     top = [ [i, j] for i,j in word_combination.iteritems()]
